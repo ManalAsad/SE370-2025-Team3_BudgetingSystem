a624b6ee3132452c9c2bd8a7ceccb4675d0e0c36
ManalAsad <manalasad18@gmail.com>
2025-04-02 23:49:38 -0700

    added budget_buddy folder
    These are for connecting to the database

---
+++ budget_buddy/src/database/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3307/budget_buddy";
+    private static final String USER = "root";
+    private static final String PASSWORD = "seproject";
+
+    public static Connection connect() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to MySQL successfully!");
+            return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Database connection failed!");
+            return null;
+        }
+    }
+}
+
---
+++ budget_buddy/src/database/TestConnection.java
@@ -0,0 +1,9 @@
+package database;
+
+public class TestConnection {
+    public static void main(String[] args){
+        DatabaseConnection.connect();
+    }
+}
+
+
