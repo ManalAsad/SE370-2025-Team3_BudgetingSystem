83837ed4ffc72c28fbc75f861c5c264033848d42
jzamiski182 <neuralverse@gmail.com>
2025-04-22 17:08:42 -0700

    Added ManualHandler.java for manual transactions

---
+++ src/main/java/org/project/util/ManualHandler.java
@@ -0,0 +1,33 @@
+package org.project.util;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.project.models.Transaction;
+
+public class ManualHandler
+{
+    private static ObservableList<Transaction> transactionData = FXCollections.observableArrayList();
+
+    public void addTransaction(Transaction transaction)
+    {
+        transactionData.add(transaction);
+    }
+
+    public Transaction getTransaction(int index)
+    {
+        if (!transactionData.isEmpty() && index >= 0)
+            return transactionData.get(index);
+
+        return null;
+    }
+
+    public boolean isEmpty()
+    {
+        return transactionData.isEmpty();
+    }
+
+    public int getSize()
+    {
+        return transactionData.size();
+    }
+}
