5fd336a40ca2d93d1f9af88af30f51c70823f57f
jzamiski182 <neuralverse@gmail.com>
2025-05-05 12:06:56 -0700

    Create RecentYear.java

---
+++ src/main/java/org/project/util/RecentYear.java
@@ -0,0 +1,16 @@
+package org.project.util;
+
+public class RecentYear
+{
+    private static String mostRecentYear = "";
+
+    public void setMostRecentYear(String year)
+    {
+        mostRecentYear = year;
+    }
+
+    public String getMostRecentYear()
+    {
+        return mostRecentYear;
+    }
+}
